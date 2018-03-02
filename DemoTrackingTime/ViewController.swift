d861a0076cdbc56cffa6023f0ba236e72803cf3e
NguyenCuong <cuong251095@gmail.com>
2018-03-02 11:18:38 +0700

    get image from video mp4

--- DemoTrackingTime/ViewController.swift
+++ DemoTrackingTime/ViewController.swift
@@ -7,17 +7,34 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    var abc: CMTime = CMTimeMake(10, 1)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        guard let path = Bundle.main.path(forResource: "video", ofType: "mp4") else { return }
+        imageView.image = generateThumbnail(url: URL(fileURLWithPath: path))
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func generateThumbnail(url: URL) -> UIImage? {
+        do {
+            let asset = AVURLAsset(url: url)
+            let imageGenerator = AVAssetImageGenerator(asset: asset)
+            imageGenerator.appliesPreferredTrackTransform = true
+            let cgImage = try imageGenerator.copyCGImage(at: abc, actualTime: nil)
+            
+            return UIImage(cgImage: cgImage)
+        } catch {
+            print(error.localizedDescription)
+            
+            return nil
+        }
     }
 
 
